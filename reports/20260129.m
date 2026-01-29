a4853a9c3ead5798cd01e598b9d287955e6380a7
Owen Koster <54965606+owenkoster@users.noreply.github.com>
2026-01-29 14:45:31 -0800

    Create 20260129.m
    Initial report for week 4

---
+++ reports/20260129.m
@@ -0,0 +1,14 @@
+1. 
+2. We discussed details of project implementation and researched different approaches for building the GUI. 
+We started work on the application design and software architecture to complete milestone 3.
+Successfully tested Python-OBD on Owens' truck to read the code for a check engine light and to clear the code. 
+3. 
+We plan on getting part 1 of milestone 3 done by the end of the day Friday. 
+We plan on getting part 2 of milestone 3 done by the end of the day Friday. 
+We plan on getting part 3 of milestone 3 done by the end of the day Friday. 
+We plan on getting part 4 of milestone 3 done by the end of the day Sunday. 
+
+Owen: Started on milestone 3, participated in project discussions, Tested Python-OBD
+Peyton: Started on milestone 3, participated in project discussions
+Tiernan: Started on milestone 3, Researched GUI approaches, participated in project discussions
+Silas: Started on milestone 3, Worked on report for week 3, participated in project discussions
