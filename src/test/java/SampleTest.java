3af01dbf0e6586f42a920ebe35b3b8887b8b24cc
owenkoster <54965606+owenkoster@users.noreply.github.com>
2026-03-05 15:52:10 -0800

    Added sample java test

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+    @Test
+    void testExample() {
+        int result = 2 + 2;
+        assertEquals(4, result);
+    }
+}
